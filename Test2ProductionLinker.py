f4be84e3b11cae44cdb008b8e0f89104719d5bca
Guilherme Volney Mota Amaral <gvma@ic.ufal.br>
2019-12-16 17:12:46 -0300

    Script changes

--- Test2ProductionLinker.py
+++ Test2ProductionLinker.py
@@ -1,27 +1,30 @@
 import os
+import sys
 
-appName = "RxJava"
-productionPath = r"D:\Pesquisa\RxJava\src\main"
-testPath = r"D:\Pesquisa\RxJava\src\test"
+path = sys.argv[1]
+appName = path.split('\\')[-1]
 
 production = {}
 test = {}
 
-for subdir, dirs, files in os.walk(productionPath):
+for subdir, dirs, files in os.walk(path):
     for file in files:
-        if(file.endswith(".java")):
+        if(file.endswith(".java") and not file.endswith("Tests.java") and not file.endswith("Test.java")):
             production[file] = os.path.join(subdir, file)
 
-
-for subdir, dirs, files in os.walk(testPath):
+for subdir, dirs, files in os.walk(path):
     for file in files:
-        if(file.endswith(".java") and (not file.endswith("TckTest.java"))): # tcktest is removed because there's no production file that corresponds.
+        if(file.endswith("Test.java") or file.endswith("Tests.java") and (not file.endswith("TckTest.java"))): # tcktest is removed because there's no production file that corresponds.
             test[file] = os.path.join(subdir, file)
 
 with open('input.csv', 'w') as f:
     for testFileName, testFilePath in test.items():
-        name = testFileName.replace('Test.java','')
+        if testFileName.endswith('Test.java'):
+            name = testFileName.replace('Test.java','')
+        elif testFileName.endswith('Tests.java'):
+            name = testFileName.replace('Tests.java','')
         for productionFileName, productionFilePath in production.items():
             if name == productionFileName.replace('.java',''):
-                print(appName +","+ testFilePath +","+ productionFilePath, file=f)
+                print(appName +","+ testFilePath +","+ productionFilePath, file = f)
+
 f.close()
